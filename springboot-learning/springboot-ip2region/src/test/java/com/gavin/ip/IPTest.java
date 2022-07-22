799d9406d7981e1741729b58e57b0a7ab1d3c059
gavinlf <gavinlf@email.com>
2022-07-22 18:51:22 +0800

    新增jackson模块

--- springboot-learning/springboot-ip2region/src/test/java/com/gavin/ip/IPTest.java
+++ springboot-learning/springboot-ip2region/src/test/java/com/gavin/ip/IPTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
 
 @SpringBootTest
 public class IPTest {
@@ -16,19 +15,19 @@ public class IPTest {
 
   @Test
   void tesIp() throws Exception {
-    System.out.println(template.btreeSearch("61.94.43.82"));
-    System.out.println(template.binarySearch("61.94.43.82"));
-    System.out.println(template.memorySearch("61.94.43.82"));
+    System.out.println(template.btreeSearch("116.228.37.198"));
+    System.out.println(template.binarySearch("116.228.37.198"));
+    System.out.println(template.memorySearch("116.228.37.198"));
     System.out.println(template.binarySearch("127.0.0.1"));
 
     // 根据IP获取对应国家
-    System.out.println(template.getCountryByIp("127.0.0.1"));
+    System.out.println(template.getCountryByIp("116.228.37.198"));
 
     // 根据IP获取对应地区
-    System.out.println(template.getRegion("114.124.146.103"));
+    System.out.println(template.getRegion("116.228.37.198"));
 
     // 根据IP获取对应地区详细信息对象
-    RegionAddress adress1 = template.getRegionAddress("113.210.53.80");
+    RegionAddress adress1 = template.getRegionAddress("116.228.37.198");
     System.out.println(adress1);
     System.out.println(RegionEnum.getByRegionAddress(adress1));
 
