a0217b0134d6c66588f000452941ba652d3f361b
gavin-james <meaganlindesy1258@gmail.com>
2023-12-13 12:41:57 +0800

    调整依赖

---
+++ springboot-learning/springboot-hellowrold/src/main/java/com/gavin/learning/HelloApplication.java
@@ -0,0 +1,4 @@
+package com.gavin.learning;
+
+public class HelloApplication {
+}
---
+++ springboot-learning/springboot-qywx/src/main/java/com/gavin/james/QywxApplication.java
@@ -0,0 +1,12 @@
+package com.gavin.james;
+
+import com.github.shuaidd.autoconfigure.EnableWeChat;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableWeChat
+public class QywxApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
